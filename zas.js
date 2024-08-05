d905771eba1c0e6632385c88be3e27a35ada0954
valhalla-09 <valhallaw09@gmail.com>
2024-08-05 01:07:53 +0100

    Create zas.js

---
+++ zas.js
@@ -0,0 +1,9 @@
+console.log("asasaiuhauhsudhasiu")
+// fetch("/zas")
+//          .then(response => response.json())
+//          .then(data => {
+//             document.querySelector("span#totalGames").innerHTML = data.jogos.length
+//             document.querySelector("span#totalApps").innerHTML = data.apps.length
+//             document.querySelector("span#totalNews").innerHTML = data.noticias.length
+//             console.log(data.jogos)
+//          })
